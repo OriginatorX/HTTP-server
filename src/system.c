5e2067b73d8e77ac57cb1f77be022161461904fe
OriginatorX <fetison50@gmail.com>
2021-09-24 18:53:18 +1000

    formating system.c

--- src/system.c
+++ src/system.c
@@ -18,7 +18,8 @@ file_t readFile(const char * path) {
         return file;
     }
 
-    if (fstat(fd, &file.info) == ERROR) perror("readFile fstat"); 
+    if (fstat(fd, &file.info) == ERROR) 
+        perror("readFile fstat"); 
     
     if (!(file.buffer = (char*) malloc(file.info.st_size)))
         perror("readFile, malloc");
@@ -26,11 +27,13 @@ file_t readFile(const char * path) {
     file.real_quant_bytes = read(fd, file.buffer, file.info.st_size);
 
     if (file.real_quant_bytes == ERROR) {
-        fprintf(stderr, "read file %s failed\n", path); return file;
+        fprintf(stderr, "read file %s failed\n", path); 
+        return file;
     }
     else if (file.real_quant_bytes == 0)
         printf("%s\n", "file reached");
-    if (close(fd) == ERROR) perror("readFile, close");
+    if (close(fd) == ERROR) 
+        perror("readFile, close");
     
     file.state_code = TRUE;
     return file;
